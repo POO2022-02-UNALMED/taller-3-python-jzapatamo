a74da53992a3b5be5120b894ef09daca9d31397a
jzapatamo <111089101+jzapatamo@users.noreply.github.com>
2022-08-30 01:27:43 -0500

    creacion clases python

---
+++ televisores/control.py
@@ -0,0 +1,46 @@
+class Control:
+    
+    def __init__(self):
+        self._tv = None
+
+
+    def _validarEnlazado(self):
+        return self._tv != None
+
+    def getTV(self):
+        return self._tv
+
+    def setTV(self, tv):
+        self._tv = tv
+
+    def enlazar(self, tv):
+        self._tv = tv
+        self._tv.setControl(self)
+
+    def turnOn(self):
+        if self._validarEnlazado():
+            self._tv.turnOn()
+    
+    def turnOff(self):
+        if self._validarEnlazado():
+            self._tv.turnOff()
+
+    def canalUp(self):
+        if self._validarEnlazado():
+            self._tv.canalUp()
+
+    def canalDown(self):
+        if self._validarEnlazado():
+            self._tv.canalDown()
+
+    def volumenUp(self):
+        if self._validarEnlazado():
+            self._tv.volumenUp()
+
+    def volumenDown(self):
+        if self._validarEnlazado():
+            self._tv.volumenDown()
+
+    def setCanal(self, canal: int):
+        if self._validarEnlazado():
+            self._tv.setCanal(canal)
---
+++ televisores/tv.py
@@ -0,0 +1,87 @@
+from televisores.marca import Marca
+from televisores.control import Control
+class TV:
+    
+    numTV: int = 0
+
+    def __init__(self, marca: Marca, estado: bool):
+        self._marca: Marca = marca
+        self._canal: int = 1
+        self._precio: int = 500
+        self._estado: bool = estado
+        self._volumen: int = 1
+        self._control: Control = None
+        TV.numTV += 1
+
+    def _canalValido(self, canal: int):
+        MAX_CANAL: int = 120
+        return self._estado and canal >= 1 and canal <= MAX_CANAL
+    
+    def _volumenValido(self, volumen: int):
+        MAX_VOLUMEN: int = 7
+        return self._estado and volumen >= 0 and volumen <= MAX_VOLUMEN
+
+
+    def getMarca(self):
+        return self._marca
+
+    def getControl(self):
+        return self._control
+
+    def getPrecio(self):
+        return self._precio
+
+    def getVolumen(self):
+        return self._volumen
+
+    def getCanal(self):
+        return self._canal
+
+    def getNumTV():
+        return TV.numTV
+
+    def getEstado(self):
+        return self._estado
+
+    def setMarca(self, marca: Marca):
+        self._marca = marca
+
+    def setControl(self, control: Control):
+        self._control = control
+
+    def setPrecio(self, precio: int):
+        self._precio = precio
+
+    def setVolumen(self, volumen: int):
+        if self._volumenValido(volumen):
+            self._volumen = volumen
+
+    def setCanal(self, canal: int):
+        if self._canalValido(canal):
+            self._canal = canal
+
+    
+    def turnOn(self):
+        self._estado = True
+    
+    def turnOff(self):
+        self._estado = False
+
+    def canalUp(self):
+        nuevoCanal: int = self._canal + 1
+        self._canal = self._canal = nuevoCanal if self._canalValido(nuevoCanal) else self._canal
+
+    def canalDown(self):
+        nuevoCanal: int = self._canal - 1
+        self._canal = self._canal = nuevoCanal if self._canalValido(nuevoCanal) else self._canal
+
+    def volumenUp(self):
+        nuevoVolumen: int = self._volumen + 1
+        self._volumen = self._volumen = nuevoVolumen if self._volumenValido(nuevoVolumen) else self._volumen
+
+    def volumenDown(self):
+        nuevoVolumen: int = self._volumen - 1
+        self._volumen = self._volumen = nuevoVolumen if self._volumenValido(nuevoVolumen) else self._volumen
+
+    
+        
